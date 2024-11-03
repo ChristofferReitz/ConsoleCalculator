e6cbb0602226e64114375d8fa1ce5d08b3c649c4
Christoffer Reit <christoffer.reitz@keyshot.com>
2024-11-03 19:35:58 +0100

    Add project files.

---
+++ ConsoleCalculator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and welcome to your cmd calculator!");
+            Console.WriteLine("start by input a number!");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Enter your mathematical operator");
+
+            string mathOperator = Console.ReadLine();
+
+                if (mathOperator != "+" || mathOperator != "-" || mathOperator != "*" || mathOperator != "/")
+                {
+                    Console.WriteLine("Invalid input try again");
+                }
+                else
+                {
+                    Console.WriteLine("Enter you second number");
+                    int secondNumer = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("The mathematical equation is: ");
+                    switch (mathOperator)
+                    {
+                        case "+":
+                            Console.WriteLine(firstNumber + secondNumer);
+                            break;
+                        case "-":
+                            Console.WriteLine(firstNumber - secondNumer);
+                            break;
+                        case "*":
+                            Console.WriteLine(firstNumber * secondNumer);
+                            break;
+                        case "/":
+                            Console.WriteLine(secondNumer / firstNumber);
+                            break;
+                    }
+                }
+        }
+    }
+}
